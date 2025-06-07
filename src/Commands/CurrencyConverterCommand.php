3f31b65e6e2a4de3cec4b896f185553614a67852
mgcodeur <mgcodeur@gmail.com>
2025-06-07 15:08:27 +0300

    style: postinstall call to action

--- src/Commands/CurrencyConverterCommand.php
+++ src/Commands/CurrencyConverterCommand.php
@@ -30,8 +30,19 @@ class CurrencyConverterCommand extends Command
         // $this->comment('Publishing laravel currency converter migrations...');
         // $this->callSilent('vendor:publish', ['--tag' => 'currency-converter-migrations']);
         // this->info('Laravel currency converter migrations published successfully.');
+        $this->line("");
 
-        $this->comment('All done!');
+        echo "\033[1;35m";
+        echo "─────────────────────────────────────────────────────────────────────────────\n";
+        echo "\033[1;33m   ★  Thank you for installing laravel-currency-converter!  ★\n\033[1;35m";
+        echo "─────────────────────────────────────────────────────────────────────────────\n";
+        echo "\033[0m";
+        echo "\033[1;37mIf you like this package, please give it a \033[1;33mstar\033[1;37m on GitHub :\n";
+        echo "\033[1;36m👉 https://github.com/mgcodeur/laravel-currency-converter\n";
+        echo "\033[0m";
+
+        $this->line("");
+        $this->line("");
 
         return self::SUCCESS;
     }
